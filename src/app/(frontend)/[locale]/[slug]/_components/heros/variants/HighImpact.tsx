dcdef755f03b1dabbf64d88521e1ef68ed3b2d03
BrandoRodriguez <brandojesusrodriguezgomez@gmail.com>
2025-02-16 17:24:01 -0500

    fix hero: resolve background

--- src/app/(frontend)/[locale]/[slug]/_components/heros/variants/HighImpact.tsx
+++ src/app/(frontend)/[locale]/[slug]/_components/heros/variants/HighImpact.tsx
@@ -13,7 +13,7 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, richText, mediaD
 
  return (
   <section className="hero">
-   <div className="container mx-auto sm:mt-50 z-10 ">
+   {/* <div className="container mx-auto sm:mt-50 z-10 ">
     <div className='min-h-screen flex flex-col justify-end max-sm:pb-10'>
      <div className='flex flex-col justify-center items-center gap-8'>
       {richText && <RichText className='sm:text-center flex flex-col gap-6 justify-center items-center' content={richText} enableGutter={false} />}
@@ -30,7 +30,7 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, richText, mediaD
       )}
      </div>
     </div>
-   </div>
+   </div> */}
    <div className="h-dvh">
     <React.Fragment>
      <Media fill imgClassName="-z-10 object-cover bg-top" priority resource={sm ? mediaDesktop ?? '' : mediaMobile ?? ''} />
